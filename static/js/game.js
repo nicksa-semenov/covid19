36ec98b7badf6e113422b38588e15e5588c488b3
Nikolay Semenov <nicksa.semenov@gmail.com>
2020-04-04 14:30:34 +0300

    fix counter

--- static/js/game.js
+++ static/js/game.js
@@ -10,7 +10,7 @@ function plural(forms, n) {
     return forms[idx] || '';
 }
 
-$(function() {
+$(function () {
     var virusCounter = 0;
     var timeCounter = 0;
     var gameDuration = 30 * 1000;
@@ -24,7 +24,10 @@ $(function() {
         var x = Math.round(Math.random() * width) + delta / 2;
         var y = Math.round(Math.random() * height) + delta / 2;
         var scale = Math.random() / 2 + 0.5;
-        return $('<div class="virus"></div>').css('transform', 'translate(' + x + 'px, ' + y + 'px' + ') scale(' + scale + ')');
+        return $('<div class="virus"></div>').css(
+            'transform',
+            'translate(' + x + 'px, ' + y + 'px' + ') scale(' + scale + ')'
+        );
     }
 
     function clearVirus() {
@@ -43,53 +46,58 @@ $(function() {
 
     function startGame(container) {
         virusCounter = 0;
+        timeCounter = 0;
         clearVirus();
         counter.text(Math.round(gameDuration / 1000));
         var viruses = createPackVirus();
         container.append(viruses);
-        var interval = setInterval(function() {
+        var interval = setInterval(function () {
             var object = createVirus();
             viruses.append(object);
         }, 0.5 * 1000);
-        var counterInterval = setInterval(function() {
+        var counterInterval = setInterval(function () {
             timeCounter += 1000;
             var diff = Math.round((gameDuration - timeCounter) / 1000);
-            counter.text(diff >= 0 ? diff :  0 );
+            counter.text(diff >= 0 ? diff : 0);
         }, 1000);
-        setTimeout(function() {
+        setTimeout(function () {
             clearInterval(interval);
             clearInterval(counterInterval);
             showEndModal();
         }, gameDuration);
     }
 
-    function showEndModal () {
+    function showEndModal() {
         $('.overlay').addClass('shown');
         $('.modal-end__text').html(
-            'Вы ликвидировали ' + virusCounter + ' ' + plural(['вирус', 'вируса', 'вирусов'], virusCounter) + 
-            '<br>Твоя скорость ' + (virusCounter / gameDuration * 1000).toFixed(1) + ' вирусов в секунду'
+            'Вы ликвидировали ' +
+                virusCounter +
+                ' ' +
+                plural(['вирус', 'вируса', 'вирусов'], virusCounter) +
+                '<br>Твоя скорость ' +
+                ((virusCounter / gameDuration) * 1000).toFixed(1) +
+                ' вирусов в секунду'
         );
     }
 
-    $('.main-page__start').on('click', function(event) {
+    $('.main-page__start').on('click', function (event) {
         event.preventDefault();
         $('.page-1').removeClass('active');
         $('.page-2').addClass('active');
         startGame($('.page-2'));
     });
 
-   $('body').on('click', '.virus', function() {
+    $('body').on('click', '.virus', function () {
         $(this).addClass('deleted').remove();
         var audio = new Audio('static/audio/hit.mp3');
         audio.play();
         virusCounter += 1;
     });
 
-    $('body').on('click', '.modal-end__start', function(event) {
+    $('body').on('click', '.modal-end__start', function (event) {
         event.preventDefault();
         $('.overlay').removeClass('shown');
         $('.page-2').removeClass('active');
         $('.page-1').addClass('active');
     });
-
 });
